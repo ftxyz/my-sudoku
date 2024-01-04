a7cd1c54f4582af67423d40d15f8d5b5a6968bb4
Fu Ting <1208511202@qq.com>
2024-01-04 09:27:27 +0800

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SudokuGrid from './SudokuGrid'
+
+const GamePage = () => {
+  // 在这里编写你的数独游戏组件代码
+  // 可以直接将第一版的React代码拷贝到这里
+
+  return (
+    <div>
+      <h1>Sudoku Game</h1>
+      <SudokuGrid />
+    </div>
+  );
+};
+
+export default GamePage;
